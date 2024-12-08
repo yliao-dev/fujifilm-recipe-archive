db443eb53d54f25d72d9abfc8f37c9eb557ffb46
yueting <ytliaorpi@gmail.com>
2024-12-08 22:03:45 +0100

    Make sure the module type is included

---
+++ client/src/types/global.d.ts
@@ -0,0 +1,21 @@
+declare module '*.css';
+// src/types/images.d.ts (or global.d.ts)
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
