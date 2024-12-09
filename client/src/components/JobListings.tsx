5ff1b00269639f8828538f539d13563df4497375
yueting <ytliaorpi@gmail.com>
2024-12-09 20:28:03 +0100

    Add spinner for loading

--- client/src/components/JobListings.tsx
+++ client/src/components/JobListings.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import JobListing from "./JobListing";
 import { BASE_URL } from "../config";
-import { Job } from "./JobListing";
+import { JobListingProps } from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
-  const [jobs, setJobs] = useState<Job[]>([]);
+  const [jobs, setJobs] = useState<JobListingProps[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -31,17 +32,20 @@ const JobListings = ({ isHome = false }) => {
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {loading ? (
-              <h2> Loading ... </h2>
-            ) : (
-              <>
-                {jobs.map((job, index) => (
-                  <JobListing key={job._id || index} body={job} />
-                ))}
-              </>
-            )}
-          </div>
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job, index) => (
+                <JobListing
+                  key={job.body._id || index}
+                  body={job.body}
+                  _id={job._id}
+                  status={job.status}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,22 @@
+import { ClipLoader } from "react-spinners";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+type SpinnerProps = {
+  loading: boolean;
+};
+
+const Spinner: React.FC<SpinnerProps> = ({ loading }) => {
+  return (
+    <ClipLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+
+export default Spinner;
