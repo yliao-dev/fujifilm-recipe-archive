b1743a1aef66136dbd02b6e7dab7a6f943ebf172
yueting <ytliaodev@gmail.com>
2025-04-12 19:26:05 +0200

    Add more data and navigation

--- client/src/pages/RecipeListPage.tsx
+++ client/src/pages/RecipeListPage.tsx
@@ -1,19 +1,63 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import RecipeCards from "../components/RecipeCards";
 import { RecipeData } from "../data/data";
 
 const RecipeListPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 12;
+  const totalPages = Math.ceil(RecipeData.length / cardsPerPage);
+
+  const startIndex = (currentPage - 1) * cardsPerPage;
+  const currentRecipes = RecipeData.slice(
+    startIndex,
+    startIndex + cardsPerPage
+  );
+
+  const handlePrev = () => {
+    if (currentPage > 1) setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) setCurrentPage((prev) => prev + 1);
+  };
+
   return (
-    <>
-      <div className="recipeList">
-        <section className="recipeList__card-grid">
-          <RecipeCards recipeData={RecipeData} />
-        </section>
-        <Link to="/" className="home__link">
-          Home Page
-        </Link>
-      </div>
-    </>
+    <div className="recipeList">
+      <section className="recipeList__card-grid">
+        <RecipeCards recipeData={currentRecipes} />
+      </section>
+
+      <section className="pagination">
+        <div>
+          <button
+            onClick={handlePrev}
+            className={currentPage === 1 ? "hidden-button" : ""}
+          >
+            ← Prev
+          </button>
+        </div>
+
+        <div>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+        </div>
+
+        <div>
+          <button
+            onClick={handleNext}
+            className={currentPage === totalPages ? "hidden-button" : ""}
+          >
+            Next →
+          </button>
+        </div>
+      </section>
+
+      <Link to="/" className="home__link">
+        Home Page
+      </Link>
+    </div>
   );
 };
 
