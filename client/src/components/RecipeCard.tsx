41566311d8140c369109d48e6b3347ac980e6c2b
yueting <ytliaodev@gmail.com>
2025-04-13 18:12:20 +0200

    Update RecipeCard.tsx

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -1,14 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { RecipeItemProps } from "../types/recipeTypes";
 
 function RecipeItem({
+  _id,
   name,
   sampleImageUrl,
   filmSimulation,
   cameraModels,
   tags,
 }: RecipeItemProps) {
+  const navigate = useNavigate();
+  const id = _id.$oid;
+
   return (
-    <div className="recipeCard">
+    <div className="recipeCard" onClick={() => navigate(`/recipes/${id}`)}>
       <img src={sampleImageUrl} alt={name} />
       <div className="recipeCard-text">
         <h2>{name}</h2>
