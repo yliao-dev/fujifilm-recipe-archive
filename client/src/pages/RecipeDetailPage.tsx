f351d06f89f2fc9bcd216399d95dbf249e030d13
yueting <ytliaodev@gmail.com>
2025-04-13 18:57:01 +0200

    more detailPage

--- client/src/pages/RecipeDetailPage.tsx
+++ client/src/pages/RecipeDetailPage.tsx
@@ -1,45 +1,45 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { RecipeData } from "../data/data";
 import NotFoundPage from "./NotFoundPage";
-import { useState } from "react";
 
 const RecipeDetailPage = () => {
   const { id } = useParams();
   const recipe = RecipeData.find((p) => p._id.$oid === id);
-  const [copied, setCopied] = useState(false);
 
   if (!recipe) return <NotFoundPage />;
 
-  // Destructure all fields except _id
-  const { _id, ...rest } = recipe;
+  const { name, cameraModels, filmSimulation, settings, creator } = recipe;
 
-  const jsonString = JSON.stringify(rest, null, 2);
-
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(jsonString);
-      setCopied(true);
-      setTimeout(() => setCopied(false), 1500);
-    } catch (err) {
-      console.error("Copy failed", err);
-    }
-  };
   return (
     <div className="recipeDetail__page">
       <section className="recipeDetail__intro">
-        <h1>{recipe.name}</h1>
-
+        <h1>{name}</h1>
         <Link to="/recipes" className="nav_button">
           Back
         </Link>
       </section>
-      <section className="copy-container">
-        <pre className="copy-box">
-          <code>{jsonString}</code>
-        </pre>
-        <button className="copy-button" onClick={handleCopy}>
-          {copied ? "Copied!" : "Copy"}
-        </button>
+
+      <section className="recipeDetail__body">
+        <h2>
+          <strong>Film Simulation:</strong> {filmSimulation}
+        </h2>
+        <h2>
+          <strong>Camera Models:</strong> {cameraModels.join(" | ")}
+        </h2>
+        <h2>
+          <strong>Settings:</strong>
+        </h2>
+        <ul>
+          {Object.entries(settings).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+
+        <h2>
+          <strong>Creator:</strong> {creator}
+        </h2>
       </section>
     </div>
   );
