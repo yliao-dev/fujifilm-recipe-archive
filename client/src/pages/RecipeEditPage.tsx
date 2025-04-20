21b025959522dacb84bc867dfdfffe9a703011f9
yueting <ytliaodev@gmail.com>
2025-04-20 20:25:00 +0200

    Update RecipeEditPage.tsx

--- client/src/pages/RecipeEditPage.tsx
+++ client/src/pages/RecipeEditPage.tsx
@@ -1,26 +1,36 @@
 import { useParams, useNavigate } from "react-router-dom";
 import RecipeForm from "../components/RecipeForm";
-import { RecipeData } from "../data/data";
 import NotFoundPage from "./NotFoundPage";
+import useUpdateItem from "../hooks/useUpdateItem";
 
 const RecipeEditPage = () => {
-  const { id } = useParams();
   const navigate = useNavigate();
 
-  const recipe = RecipeData.find((item) => item._id.$oid === id);
+  const { id } = useParams();
+  if (!id) {
+    return <NotFoundPage />;
+  }
+  console.log(id);
+  const { data: RecipeData, error } = useUpdateItem(id);
+
+  if (!RecipeData) return <NotFoundPage />;
 
   const handleSubmit = (updatedData: any) => {
     // Update logic here
     navigate(`/recipes/${updatedData._id.$oid}`);
   };
 
-  if (!recipe) return <NotFoundPage />;
+  if (!RecipeData) return <NotFoundPage />;
 
   return (
     <div className="recipeCreate__page">
       <h1>Edit Film Recipe</h1>
       <hr className="recipeList__divider" />
-      <RecipeForm mode="edit" initialData={recipe} onSubmit={handleSubmit} />
+      <RecipeForm
+        mode="edit"
+        initialData={RecipeData}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 };
