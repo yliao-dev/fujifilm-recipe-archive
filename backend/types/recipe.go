aa8ce00237b69301904f6a87b5a349ff9a649874
yueting <ytliaodev@gmail.com>
2025-04-21 22:27:11 +0200

    Update recipe.go

--- backend/types/recipe.go
+++ backend/types/recipe.go
@@ -4,30 +4,29 @@ package types
 
 type Recipe struct {
 	ID             string    `bson:"_id,omitempty" json:"_id"`
-	Name           string    `json:"name"`
-	CameraModels   []string  `json:"camera_models"`
-	FilmSimulation string    `json:"film_simulation"`
-	Creator        string    `json:"creator"`
-	Tags           []string  `json:"tags"`
-	Notes          string    `json:"notes"`
-	SampleImageURL string    `json:"sample_image_url"`
-	CreatedAt      string    `json:"created_at"`
-	Settings       Settings  `json:"settings"`
+	Name           string    `bson:"name" json:"name"`
+	CameraModels   []string  `bson:"camera_models" json:"camera_models"`
+	FilmSimulation string    `bson:"film_simulation" json:"film_simulation"`
+	Creator        string    `bson:"creator" json:"creator"`
+	Tags           []string  `bson:"tags" json:"tags"`
+	Notes          string    `bson:"notes" json:"notes"`
+	SampleImageURL string    `bson:"sample_image_url" json:"sample_image_url"`
+	CreatedAt      string    `bson:"created_at" json:"created_at"`
+	Settings       Settings  `bson:"settings" json:"settings"`
 }
 
-
 type Settings struct {
-	GrainEffect            string `json:"grain_effect"`
-	ColorChromeEffect      string `json:"color_chrome_effect"`
-	ColorChromeFxBlue      string `json:"color_chrome_fx_blue"`
-	WhiteBalance           string `json:"white_balance"`
-	DynamicRange           string `json:"dynamic_range"`
-	Highlight              string `json:"highlight"`
-	Shadow                 string `json:"shadow"`
-	Color                  string `json:"color"`
-	Sharpness              string `json:"sharpness"`
-	NoiseReduction         string `json:"noise_reduction"`
-	Clarity                string `json:"clarity"`
-	ISO                    string `json:"iso"`
-	ExposureCompensation   string `json:"exposure_compensation"`
+	GrainEffect          string `bson:"grain_effect" json:"grain_effect"`
+	ColorChromeEffect    string `bson:"color_chrome_effect" json:"color_chrome_effect"`
+	ColorChromeFxBlue    string `bson:"color_chrome_fx_blue" json:"color_chrome_fx_blue"`
+	WhiteBalance         string `bson:"white_balance" json:"white_balance"`
+	DynamicRange         string `bson:"dynamic_range" json:"dynamic_range"`
+	Highlight            string `bson:"highlight" json:"highlight"`
+	Shadow               string `bson:"shadow" json:"shadow"`
+	Color                string `bson:"color" json:"color"`
+	Sharpness            string `bson:"sharpness" json:"sharpness"`
+	NoiseReduction       string `bson:"noise_reduction" json:"noise_reduction"`
+	Clarity              string `bson:"clarity" json:"clarity"`
+	ISO                  string `bson:"iso" json:"iso"`
+	ExposureCompensation string `bson:"exposure_compensation" json:"exposure_compensation"`
 }
