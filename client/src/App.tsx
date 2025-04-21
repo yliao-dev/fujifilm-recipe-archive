ce26ae73bf14c61cf5074a7393bd6deb1da76af1
yueting <ytliaodev@gmail.com>
2025-04-21 13:45:23 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import RecipeListPage from "./pages/RecipeListPage";
 import RecipeDetailPage from "./pages/RecipeDetailPage";
 import RecipeCreatePage from "./pages/RecipeCreatePage";
 import RecipeEditPage from "./pages/RecipeEditPage";
-import NotFoundPage from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -24,7 +24,7 @@ const App = () => {
         <Route path="/add-recipe" element={<RecipeCreatePage />} />
         <Route path="/edit-recipe/:id" element={<RecipeEditPage />} />
 
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     )
   );
